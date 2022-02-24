74a2f0ec6b32bda6c8a7e52d119ef7fe0878bc98
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-02-24 19:16:24 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by mitr on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
