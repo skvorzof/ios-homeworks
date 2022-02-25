aba8ca9c042d30f97f380373f1463f57e8a2f78a
Dima Skvortsov <4594651+skvorzof@users.noreply.github.com>
2022-02-25 17:42:48 +0300

    Добавил ProfileView.swift

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by mitr on 25.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var profileAvatar: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var prifileBirthday: UILabel!
+    @IBOutlet weak var profileCity: UILabel!
+    @IBOutlet weak var profileDescription: UITextView!
+}
